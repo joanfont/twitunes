1d09a6a2fb8b07f85f97d8c05397b16234943b0e
Joan Font <joanfont@gmail.com>
2015-02-24 21:45:58 +0100

    support to unicode strings

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import hashlib
 
 def md5(string):
-	md5 = hashlib.md5(string.encode())
+	md5 = hashlib.md5(string)
 	return md5.hexdigest()
--- worker.py
+++ worker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from Foundation import *
 from ScriptingBridge import *
@@ -14,25 +14,40 @@ twitter = twitter.Api(**config.TWITTER_CONFIG)
 
 previous = None
 
+def construct_string(info):
+  base = u'#nowplaying'
+  string = u'{base}'.format(base = base)
+  if info.get('title'):
+    string += u' {title}'.format(title = info.get('title'))
+
+  if info.get('artist'):
+    string += u' - {artist}'.format(artist = info.get('artist'))
+
+  if info.get('album'):
+    string += u' [{album}]'.format(album = info.get('album'))
+
+  return string if string != base else None
+
 def itunes_checker():
 
-	global previous
+  global previous
 
-	title = iTunes.currentTrack().name()
-	artist = iTunes.currentTrack().artist()
-	album = iTunes.currentTrack().album()
+  title = iTunes.currentTrack().name()
+  artist = iTunes.currentTrack().artist()
+  album = iTunes.currentTrack().album()
 
-	info = {'title': title, 'artist': artist, 'album': album}
+  info = {'title': title, 'artist': artist, 'album': album}
 
-	song = '#nowplaying {title} - {artist} [{album}]'.format(**info)
-	hashed = md5(song)
+  song = construct_string(info).encode('utf8')
 
-	if hashed != previous:
-		previous = hashed
-		twitter.PostUpdate(song)
+  if song:
+    hashed = md5(song)
+    if hashed != previous:
+      previous = hashed
+      twitter.PostUpdate(song)
 
 
-		
+    
 
 def worker(interval, fnx):
   threading.Timer(interval, worker, [interval, fnx]).start()
@@ -40,4 +55,4 @@ def worker(interval, fnx):
 
 
 if __name__ == '__main__':
-	worker(1, itunes_checker)
+  worker(1, itunes_checker)
